15d35a26be829e027ca1fad6d3e3bcae5b7ce2a6
Daeynasvistas <netrapida@live.com.pt>
2019-01-14 19:54:01 +0000

    DaoUser

---
+++ app/src/main/java/pt/ipg/taxiapp/UserDao.java
@@ -0,0 +1,30 @@
+package pt.ipg.taxiapp;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface UserDao {
+
+    @Insert
+    void insert(User user);
+
+    @Update
+    void update(User user);
+
+    @Delete
+    void delete(User user);
+
+    @Query("DELETE FROM user_table")
+    void deleteAllUsers();
+
+    @Query("SELECT * FROM USER_TABLE")
+    LiveData<List<User>> getAllUsers();
+
+}
