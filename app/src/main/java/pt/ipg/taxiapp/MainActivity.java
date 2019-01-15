4be6eec64a80f5a8b345a9520d76b38f7d03c24c
daeynasvistas <netrapida@live.com.pt>
2019-01-15 11:15:28 +0000

    Update MainActivity.java

--- app/src/main/java/pt/ipg/taxiapp/MainActivity.java
+++ app/src/main/java/pt/ipg/taxiapp/MainActivity.java
@@ -1,13 +1,30 @@
 package pt.ipg.taxiapp;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private UserViewModel userViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userViewModel = ViewModelProviders.of(this).get(UserViewModel.class);
+        userViewModel.getAllNotes().observe(this, new Observer<List<User>>() {
+
+            @Override
+            public void onChanged(@Nullable List<User> users) {
+                //update RecyclerView
+                Toast.makeText(MainActivity.this, "onChanged", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
